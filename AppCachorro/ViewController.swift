7addccd184011385c685d7da116807bc2c7c28e7
fstufop <fstufop@gmail.com>
2021-08-05 22:02:16 -0300

    App pronto

--- AppCachorro/ViewController.swift
+++ AppCachorro/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        let idade = Int(campoIdadeCachorro.text!)!*7
+            legendaResultado.text = "A idade do cão é " + String(idade) + " de idade"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
